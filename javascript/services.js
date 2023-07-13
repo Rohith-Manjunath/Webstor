6308993ccbe4aeaec3e9c1bce5868c65e5a0e4d2
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-07-13 23:52:33 +0530

    Fixed some bugs

--- javascript/services.js
+++ javascript/services.js
@@ -30,7 +30,7 @@ number_spans2.addEventListener("click", () => {
   our_process_description.textContent =
     "In this step, the development team evaluates the project requirements and conducts a thorough analysis of the scope, timeline, and resources needed for successful implementation. They create a detailed plan outlining the development process, milestones, and any potential challenges.";
 
-  progress_bar.style.width = "10.5rem";
+  progress_bar.style.width = "10rem";
   progress_bar.style.transition = "all 1s";
 });
 number_spans3.addEventListener("click", () => {
@@ -39,7 +39,7 @@ number_spans3.addEventListener("click", () => {
   our_process_description.textContent =
     "Once the planning phase is complete, the design and development phase begins. Designers create the visual layout, user interface, and overall aesthetic of the website, while developers write the code and build the website's functionality based on the approved design. This step involves coding, integrating necessary features, and ensuring compatibility across different devices and browsers";
 
-  progress_bar.style.width = "21rem";
+  progress_bar.style.width = "20rem";
   progress_bar.style.transition = "all 1s";
 });
 number_spans4.addEventListener("click", () => {
@@ -48,7 +48,7 @@ number_spans4.addEventListener("click", () => {
   our_process_description.textContent =
     "After the initial development is completed, the website goes through a rigorous quality testing phase. This involves checking for any bugs, errors, or usability issues. The development team performs various tests to ensure the website is functioning properly and meeting the client's requirements. Once the website passes all quality checks, it is ready for delivery to the client";
 
-  progress_bar.style.width = "31.5rem";
+  progress_bar.style.width = "30rem";
   progress_bar.style.transition = "all 1s";
 });
 
@@ -58,7 +58,7 @@ number_spans5.addEventListener("click", () => {
   our_process_description.textContent =
     "Once the website is delivered, ongoing maintenance and testing are crucial to ensure its optimal performance. This step involves monitoring the website for any issues, applying updates and security patches, and performing regular backups. Testing may also continue to identify and fix any bugs or errors that arise after the initial delivery";
 
-  progress_bar.style.width = "42rem";
+  progress_bar.style.width = "40rem";
   progress_bar.style.transition = "all 1s";
 });
 
@@ -67,6 +67,6 @@ number_spans6.addEventListener("click", () => {
   our_process_heading.textContent = "Delivery";
   our_process_description.textContent =
     "In this final step, the completed and thoroughly tested website is officially handed over to the client. The development team assists with the deployment of the website to the client's hosting environment or makes it live on the desired domain. At this stage, any remaining documentation, training, or support is provided to ensure the client's seamless transition and understanding of the website.";
-  progress_bar.style.width = "52.5rem";
+  progress_bar.style.width = "50rem";
   progress_bar.style.transition = "all 1s";
 });
